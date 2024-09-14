33cd7dee9e2574875580230a5fa22a718916d703
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 17:38:10 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import WhatsAppChatbot from '../components/WhatsAppChatbot';
+
+export default function Home() {
+  return (
+    <div>
+      <WhatsAppChatbot />
+    </div>
+  );
+}
