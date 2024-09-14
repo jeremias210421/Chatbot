96aba44bf0caa8eaa21b426c2d3c18c274c99663
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 17:38:56 -0300

    Create WhatsAppChatbot.js

---
+++ components/WhatsAppChatbot.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Textarea } from '@/components/ui/textarea';
+
+// O resto do código permanece o mesmo
+const defaultRoutines = [
+  // ... (mantenha os defaultRoutines como estavam)
+];
+
+const WhatsAppChatbot = () => {
+  // ... (todo o código do componente permanece o mesmo)
+};
+
+export default WhatsAppChatbot;
