36eb60d9633256c3b18130c4fd4702111311a7da
jeremias210421 <jeremias210421@gmail.com>
2024-09-14 22:25:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const path = require('path');
 
-module.exports = nextConfig
+module.exports = {
+  // Configurações do webpack
+  webpack: (config, { dev }) => {
+    // Configurações adicionais do webpack podem ser feitas aqui
+
+    // Configuração de alias para importação de componentes
+    config.resolve.alias['@components'] = path.join(__dirname, 'components');
+
+    return config;
+  },
+
+  // Configurações do servidor
+  serverRuntimeConfig: {
+    // Configurações que só devem ser acessíveis no lado do servidor
+  },
+
+  // Configurações do cliente
+  publicRuntimeConfig: {
+    // Configurações que devem ser acessíveis tanto no lado do servidor quanto no cliente
+  },
+
+  // Configurações de fontes externas
+  images: {
+    domains: ['example.com', 'anotherdomain.com'], // Domínios de onde as imagens podem ser carregadas
+  },
+
+  // Outras configurações do Next.js
+  // ...
+};
